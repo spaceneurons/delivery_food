0f808e1e0455c3cc4af759503d9132e44d30f88f
Margarita <rushevamar@mail.ru>
2021-11-07 17:38:42 +0300

    Динамическая отрисовка карточек

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,48 @@
-const restourant = 'food-band'
+const menu = document.querySelector(".cards-menu");
 
-const renserItems = (data) => {
-    console.log(data);
-};
+const restaurantTitle = document.querySelector(".restaurant-title");
+const restaurantRating = document.querySelector(".rating");
+const restaurantPrice = document.querySelector(".price");
+const restaurantCategory = document.querySelector(".category");
 
-fetch(`./db/${restourant}.json`)
-  .then((response) => response.json())
-  .then((data) => {
-    renserItems(data);
-  })
-  .catch((error) => {
-    console.log(error);
-  })
+if (localStorage.getItem("restaurant")) {
+  const restaurant = JSON.parse(localStorage.getItem("restaurant"));
+  fetch(`./db/${restaurant.products}`)
+    .then((response) => response.json())
+    .then((data) => renderCards(data))
+    .catch((error) => console.log(error));
+
+  restaurantTitle.textContent = restaurant.name;
+  restaurantRating.textContent = restaurant.stars;
+  restaurantPrice.textContent = `От ${restaurant.price} ₽`;
+  restaurantCategory.textContent = restaurant.kitchen;
+} else {
+  window.location.href = "/";
+}
+
+function renderCards(cards) {
+  cards.forEach(({ image, name, description, price }) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    card.innerHTML = `
+    <img src="${image}" alt="image" class="card-image" />
+    <div class="card-text">
+      <div class="card-heading">
+        <h3 class="card-title card-title-reg">${name}</h3>
+      </div>
+      <div class="card-info">
+        <div class="ingredients">${description}</div>
+      </div>
+      <div class="card-buttons">
+        <button class="button button-primary button-add-cart">
+          <span class="button-card-text">В корзину</span>
+          <span class="button-cart-svg"></span>
+        </button>
+        <strong class="card-price-bold">${price} ₽</strong>
+      </div>
+    </div>
+  `;
+    menu.append(card);
+  });
+}
--- js/partners.js
+++ js/partners.js
@@ -1,13 +1,42 @@
-const renserItems = (data) => {
-  console.log(data);
-};
+const url = String(document.location).replace("/index.html", "");
 
-fetch("https://delivery-9c0f2-default-rtdb.firebaseio.com/db/partners.json")
+const restaurants = document.querySelector(".cards-restaurants");
+
+function renderRestaurants(cards) {
+  cards.forEach((item) => {
+    const { products, image, name, time_of_delivery, stars, price, kitchen } =
+      item;
+    const a = document.createElement("a");
+    a.setAttribute("href", url + "/restaurant.html");
+    a.classList.add("card", "card-restaurant");
+    a.dataset.products = products;
+
+    a.innerHTML = `
+      <img src="${image}" alt="image" class="card-image" />
+      <div class="card-text">
+        <div class="card-heading">
+          <h3 class="card-title">${name}</h3>
+          <span class="card-tag tag">${time_of_delivery} мин</span>
+        </div>
+        <div class="card-info">
+          <div class="rating">${stars}</div>
+          <div class="price">От ${price} ₽</div>
+          <div class="category">${kitchen}</div>
+        </div>
+      </div>
+    `;
+
+    a.onclick = (e) => {
+      e.preventDefault();
+      localStorage.setItem("restaurant", JSON.stringify(item));
+      window.location.href = url + "/restaurant.html";
+    };
+
+    restaurants.append(a);
+  });
+}
+
+fetch("./db/partners.json")
   .then((response) => response.json())
-  .then((data) => {
-    renserItems(data);
-  })
-  .catch((error) => {
-    console.log(error);
-  })
-  
+  .then((data) => renderRestaurants(data))
+  .catch((error) => console.log(error));
